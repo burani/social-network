78c5a6d8dd5130b4f992ccc3f45fae04bda19ee1
Artem Egorov <doulikehurtingotherpeople@gmail.com>
2020-07-01 19:03:48 +0300

    Updates

---
+++ precache-manifest.fbf22664ad8fee69fa846859f0136821.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e22dd6fe6b5300cc16352ef64dc4743",
+    "url": "/social-network/index.html"
+  },
+  {
+    "revision": "69b68cbb785b73c34740",
+    "url": "/social-network/static/css/3.39536b2f.chunk.css"
+  },
+  {
+    "revision": "1f62be7c29bd5a19f252",
+    "url": "/social-network/static/css/4.1b20db63.chunk.css"
+  },
+  {
+    "revision": "b713a9ae0e142e9e3ecf",
+    "url": "/social-network/static/css/main.edce402d.chunk.css"
+  },
+  {
+    "revision": "f9a227f031a9ed266bce",
+    "url": "/social-network/static/js/2.f8e04b47.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/social-network/static/js/2.f8e04b47.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69b68cbb785b73c34740",
+    "url": "/social-network/static/js/3.e9a53309.chunk.js"
+  },
+  {
+    "revision": "1f62be7c29bd5a19f252",
+    "url": "/social-network/static/js/4.1fe22a3a.chunk.js"
+  },
+  {
+    "revision": "b713a9ae0e142e9e3ecf",
+    "url": "/social-network/static/js/main.9f95a605.chunk.js"
+  },
+  {
+    "revision": "2b1c93e81c712e6f5389",
+    "url": "/social-network/static/js/runtime-main.4e26d5d9.js"
+  },
+  {
+    "revision": "7b5de5e0988f85659f3ab73d9b464ccc",
+    "url": "/social-network/static/media/Spinner.7b5de5e0.svg"
+  }
+]);
